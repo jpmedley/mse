3c1ad52130d58d1c74c88aaa97f21ca506e00ed6
Joseph Medley <jmedley@google.com>
2017-08-22 13:51:03 -0700

    Improve makevids.sh.

--- media/makevids.sh
+++ media/makevids.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# Usage makevids.sh list.csv inputfile outputbase
+# Example:
+#   ./makevids.sh segs.csv source droid
+#
+#   Looks for a file named source.mpr runs it through the process
+#   in list.csv and spits out files named dorid_v_000.mp4,
+#   droid_a_000.m4a, droid_v_000.webm, droid_a_000.webm and etc..
 
 echo ============================================================
 echo Starting Conversion
@@ -11,7 +18,7 @@ while read line || [[ -n "$line" ]]; do
     echo ------------------------------------------------------------
     echo "$line"
     echo ------------------------------------------------------------
-    $line </dev/null
+    eval $line </dev/null
     # echo
   fi
 done < "$1"
