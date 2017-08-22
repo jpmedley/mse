529eb9ede10fd2d6b1095cc9c8df198a0cf69d8e
Joseph Medley <jmedley@google.com>
2017-08-22 12:54:11 -0700

    Make getValidMimeType() handle more packagings.

--- helpers.js
+++ helpers.js
@@ -1,8 +1,21 @@
 'use strict';
 
-function getValidMimeType() {
-  var mimeTypes = ['video/webm; codecs="opus, vp9"',
-                   'video/mp4; codecs="avc1.42E01E, mp4a.40.2"'];
+function getValidMimeType(mediaType) {
+  mediaType = mediaType || "muxed";
+  const mimeCatalog = {
+    "codecs": {
+      "video": [
+        "video/webm; codecs=\"vp9\"",
+        "video/mp4; codecs=\"mp4a.40.2\""
+      ],
+      "muxed": [
+        "video/webm; codecs=\"opus, vp9\"",
+        "video/mp4; codecs=\"avc1.42E01E, mp4a.40.2\""
+      ]
+    }
+  }
+  let mimeTypes = mimeCatalog.codecs[mediaType];
+
   const fileExtRe = /\/(\w+);/;
   for (var t in mimeTypes) {
     if (MediaSource.isTypeSupported(mimeTypes[t])) {
