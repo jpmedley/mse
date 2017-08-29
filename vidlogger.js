95a2854082734180e2b3f47b0ce0aaabf3aba45f
Joseph Medley <jmedley@google.com>
2017-08-29 13:35:20 -0700

    Make showBuffers() not run perpetually.

--- vidlogger.js
+++ vidlogger.js
@@ -3,11 +3,13 @@
 function showBuffers() {
   const video = document.querySelector('video');
   setInterval(() => {
-    console.log("currentTime = " + video.currentTime + ", dropped = " 
-      + video.webkitDroppedFrameCount + ", decoded = " + video.webkitDecodedFrameCount + "\n");
-    let buffered = video.buffered;
-    for (let i = 0; i < buffered; i++) {
-      console.log("\tbuffered[" + i + "]: " + buffered.start(i) + "-" + buffered.end(i) + "\n")
+    if (!video.paused) {
+      console.log("currentTime = " + video.currentTime + ", dropped = " 
+        + video.webkitDroppedFrameCount + ", decoded = " + video.webkitDecodedFrameCount + "\n");
+      let buffered = video.buffered;
+      for (let i = 0; i < buffered; i++) {
+        console.log("\tbuffered[" + i + "]: " + buffered.start(i) + "-" + buffered.end(i) + "\n")
+      }
     }
   }, 1000);
 }
